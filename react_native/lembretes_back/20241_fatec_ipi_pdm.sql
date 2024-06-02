a6b0ea466ca71b3fc3a13a4600006059aa222a4b
wagner lima <wagner.lima.andrade@alumni.usp.br>
2024-06-02 20:53:05 -0300

    finalização back

---
+++ react_native/lembretes_back/20241_fatec_ipi_pdm.sql
@@ -0,0 +1,27 @@
+-- Active: 1716598823947@@mysql-lembretes-lembretes.k.aivencloud.com@22418@20241_fatec_ipi_pdm
+-- Active: 1715685873518@@localhost@5432@20241_fatec_ipi_pdmn@public
+
+SELECT * FROM tb_lembrete;
+
+
+INSERT INTO tb_lembrete (texto) VALUES
+('Ver um filme');
+
+
+INSERT INTO
+    tb_lembrete (texto)
+VALUES ('Ir à feira'),
+    ('Encerrar a aula');
+
+
+--postgresql
+CREATE TABLE tb_lembrete(
+  id SERIAL PRIMARY KEY,
+  texto VARCHAR(1000) NOT NULL
+);
+
+--mysql
+CREATE TABLE tb_lembrete(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  texto VARCHAR(1000) NOT NULL
+);
